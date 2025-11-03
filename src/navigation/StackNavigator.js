3068ff9c2b048272ce2d9987c1c41d94f6e4672c
Briana_ML <brianamikk2@gamil.com>
2025-11-03 14:43:10 -0300

    no se

--- src/navigation/StackNavigator.js
+++ src/navigation/StackNavigator.js
@@ -1,4 +1,4 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import Register from "../screens/Register";
