a7e57723b4f33c8a0b58e8de1ca2de7bc2e390f5
Briana_ML <brianamikk2@gamil.com>
2025-11-03 10:47:24 -0300

    empece con la navegacion

---
+++ src/navigation/StackNavigator.js
@@ -0,0 +1,35 @@
+import React, {Component} from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import Register from "../screens/Register";
+import Login from "../screens/Login";
+
+import TabNavigator from "./TabNavigator";
+const Stack = createNativeStackNavigator();
+
+export default class StackNavigator extends Component {
+    render() {
+        return (
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen 
+                    name="Register"
+                    component={Register}
+                    options={{ headerShown: false }} 
+                />
+                <Stack.Screen 
+                    name="Login"
+                    component={Login} 
+                    options={{ headerShown: false }}
+                />
+                <Stack.Screen 
+                    name="TabNavigator" 
+                    component={TabNavigator} 
+                    options={{ headerShown: false }}
+                />
+            </Stack.Navigator>
+        </NavigationContainer>
+        )
+    }
+}
