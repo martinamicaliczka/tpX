54db96d5fd479d58f2ebad2f14cad265f37c69f4
Martina Michaliczka <michamartu05@gmail.com>
2025-11-03 14:23:10 -0300

    register con css y creo que sin errores

--- src/screens/Register.js
+++ src/screens/Register.js
@@ -16,10 +16,12 @@ export default class Register extends Component {
     console.log(`Creando usuario: ${username} Password: ${password} Email: ${email}`);
     if(username.length > 3 && password.length > 5 && email.includes("@")){
       auth.createUserWithEmailAndPassword(email, password)
-      db.collection('users').add({
+      .then((res) => {
+        return db.collection('users').add({
         owner: auth.currentUser.email,
         username: username,
         createdAt: Date.now(),
+    });
       })
       .then((user) => {
           this.props.navigation.navigate('Login'); //redireccionamos al login
@@ -27,17 +29,16 @@ export default class Register extends Component {
       .catch((err) => {
         console.log(`Error en la creacion de user, err: ${err}`);
       });
-    }
-  }
+    }}
 
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.titulo}> Registrate </Text>
+        <Text style={styles.titulo}> Crea tu cuenta </Text>
         <View>
           <TextInput
             style={styles.input}
-            keyboardType='email-address'
+            keyboardType='username'
             placeholder='Username'
             onChangeText={(text) => this.setState({username:text})}
             value={this.state.username}
@@ -45,14 +46,14 @@ export default class Register extends Component {
           <TextInput
             style={styles.input}
             keyboardType='email-address'
-            placeholder='Ingrese su email'
+            placeholder='Correo electrónico'
             onChangeText={(text) => this.setState({email:text})}
             value={this.state.email}
           />
           <TextInput
             style={styles.input}
-            keyboardType='email-address'
-            placeholder='Password'
+            keyboardType='password'
+            placeholder='Contraseña'
             onChangeText={(text) => this.setState({password:text})}
             value={this.state.password}
             secureTextEntry={true}
@@ -60,11 +61,11 @@ export default class Register extends Component {
           <Pressable 
           onPress={() => this.submit(this.state.username, this.state.password, this.state.email)}
           >
-            <Text style={styles.boton}>Enviar registro</Text>
+            <Text style={styles.boton}>Siguiente</Text>
           </Pressable>
         </View>
         <Pressable onPress={() => this.props.navigation.navigate('Login')}>
-            <Text>¿Tenes una cuenta?</Text>
+            <Text style={styles.textoNormal}>¿Tenes una cuenta?</Text>
             <Text style={styles.link}>Logueate</Text>
         </Pressable>
       </View>
@@ -74,40 +75,48 @@ export default class Register extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    paddingTop: '20%',
     alignItems: 'center',
     width: '100%',
+    backgroundColor: 'black'
   },
   titulo: {
     fontSize: 30,
     marginBottom: 20,
     fontWeight: 'bold',
+    color:'white',
   },
   link: {
     fontSize: 15,
-    backgroundColor: 'rgb(55 144 204)',
+    backgroundColor: '#45484bff',
     width: 300,
     justifyContent: 'center',
     textAlign: 'center',
     padding: 10,
     color: 'black',
-    borderRadius: 5,
+    borderRadius: 13,
   },
   input: {
     height: 40,
-    borderColor: 'gray',
+    borderColor: 'black',
+    borderBlockEndColor: '#2F3336',
+    color: '#71767B',
     borderWidth: 1,
     marginBottom: 10,
     paddingHorizontal: 10,
     width: 300,
   },
   boton: {
-    backgroundColor: '#28a745',
-    paddingHorizontal:10,
+    backgroundColor: '#71767B',
+    paddingHorizontal:3,
     paddingVertical: 6,
     textAlign: 'center',
-    borderRadius: 4,
-    border: '1px solid #28a745',
+    borderRadius: 13,
     marginBottom: 10,
   },
+  textoNormal:{
+    textAlign:'center',
+    color: '#71767B',
+    paddingBottom: '4px'
+  }
 });
