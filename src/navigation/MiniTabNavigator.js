537ef6c38e1028bf3437e510ac71f01374bd5327
Briana_ML <brianamikk2@gamil.com>
2025-11-03 11:23:18 -0300

    listo navegacion

---
+++ src/navigation/MiniTabNavigator.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import ComentarPost from "../screens/ComentarPost";
+import HomePage from "../screens/HomePage";
+
+const Stack = createNativeStackNavigator();
+
+export default class MiniTabNavigator extends Component {
+    render() {
+        return (
+            <Stack.Navigator>
+                <Stack.Screen 
+                    name="HomePage" 
+                    component={HomePage} 
+                    options={{ headerShown: false }}
+                />
+                <Stack.Screen 
+                    name="ComentarPost" 
+                    component={ComentarPost} 
+                    options={{ headerShown: false }}
+                />
+            </Stack.Navigator>
+        )}
+    }
--- src/navigation/TabNavigator.js
+++ src/navigation/TabNavigator.js
@@ -1,12 +1,46 @@
-import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import React from 'react'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+import Profile from '../screens/Profile';
+import Comments from '../screens/Comments';
+import MiniTabNavigator from './MiniTabNavigator';
+
+import Entypo from '@expo/vector-icons/Entypo';
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+import Feather from '@expo/vector-icons/Feather';
+
+const Tab = createBottomTabNavigator();
 
 export default class TabNavigator extends Component {
     render() {
         return (
-        <View>
-            <Text> textInComponent </Text>
-        </View>
+            <Tab.Navigator>
+                <Tab.Screen 
+                    name="MiniTabNavigator" 
+                    component={MiniTabNavigator} 
+                    options={
+                        {tabBarIcon: () => <Entypo name="home" size={24} color="white" /> ,
+                        headerShown: false,
+                        tabBarLabel:"HomePage"
+                    }}
+                />
+                <Tab.Screen 
+                    name="Profile" 
+                    component={Profile} 
+                    options={
+                        {tabBarIcon: () => <MaterialIcons name="person-outline" size={24} color="white" /> ,
+                        headerShown: false
+                    }}
+                />
+                <Tab.Screen 
+                    name="Comments" 
+                    component={Comments} 
+                    options={
+                        {tabBarIcon: () => <Feather name="message-circle" size={24} color="white" /> ,
+                        headerShown: false
+                    }}
+                />
+            </Tab.Navigator>
         )
     }
 }
