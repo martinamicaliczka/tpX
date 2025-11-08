c3730c85631e969349260e729802ef5e71338d52
pilarudesa <pilargomez@udesa.edu.ar>
2025-11-08 17:15:58 -0300

    empezando comentarios

--- src/components/FormComentar.js
+++ src/components/FormComentar.js
@@ -1,14 +1,37 @@
 import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import { Text, TextInput, View, StyleSheet } from 'react-native'
 
 export class FormComentar extends Component {
+  constructor(props){
+    super(props)
+    this.state={
+      comentario: '',
+      error: false
+    }
+  }
   render() {
     return (
-      <View>
-        <Text> comenta </Text>
+      <View style={styles.container}>
+        <Text></Text>
+        <TextInput
+        keyboardType='default'
+        placeholder='Postea tu respuesta'
+        onChangeText={text => this.setState({comentario:text})}
+        value={this.state.comentario}
+        />
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: '20%',
+    alignItems: 'center',
+    width: '100%',
+    backgroundColor: 'black'
+  },
+})
 
 export default FormComentar
