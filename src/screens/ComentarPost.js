3745e0bf5027583e02f0e82a7c346ffd7b42638f
pilarudesa <pilargomez@udesa.edu.ar>
2025-11-10 10:18:35 -0300

    indentacion

--- src/screens/ComentarPost.js
+++ src/screens/ComentarPost.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Text, View, FlatList, ActivityIndicator, StyleSheet } from 'react-native'
+import { Text, View, FlatList, StyleSheet } from 'react-native'
 import { db, auth } from "../firebase/config"
 import firebase from "firebase"
 import FormComentar from '../components/FormComentar'
